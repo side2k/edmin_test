695694a06c28595fc9bd86b3bac16a824cb42656
Leonid Amirov <leonid.amirov@gmail.com>
2013-07-27 00:34:32 +0400

    basic project structure

---
+++ edmin_test/urls/base.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+from edmin_test.views import index
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', index, name='index'),
+    url(r'^admin/', include(admin.site.urls)),
+)
